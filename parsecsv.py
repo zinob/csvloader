f63dcf5542462c0eb16a8c0031300cbbf4da472c
Simon Albinsson <zinobg@zxz.se>
2015-03-04 23:26:48 +0100

    Wrote function to split row
    Wrote function to naivley split a row. Wrote about 20 times as much
    documentation and a crude test.

--- parsecsv.py
+++ parsecsv.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 import doctest
 
+def splitrow(row,sep=","):
+	"""Naivly splits a row.
+	
+	row:the row to be splitted
+	sep:the character to split on, this can not be quoted in
+	   the row "foo\\,bar" will do nothing more than create
+		the splits "foo\\" and "bar"
+			bar
+	"""
+	#silly function? yes, but one might make it smarter later..
+	return row.split(sep)
+
+def _test_rowsplit():
+	"""
+	>>> splitrow("foo,bar,mooh,cow")
+	['foo', 'bar', 'mooh', 'cow']
+	"""
 if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
